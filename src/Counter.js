2872cb9f02ffc44b32686713082bc55b29a2eb19
leesoo <isoo7510@gmail.com>
2018-12-19 16:59:11 +0900

    component lifecycle

--- src/Counter.js
+++ src/Counter.js
@@ -1,38 +1,63 @@
 import React from 'react';
 
-class Counter extends React.Component {
+class Counter  extends React.Component {
+    state = {
+        number: 0
+    }
+
     constructor(props) {
         super(props);
-        this.state = {
-            number: 0
-        }
+        console.log('constructor');
+    }
+
+    componentWillMount() {
+        console.log('componentWillMount (deprecated)');
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount');
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        // 5의 배수라면 리렌더링 하지 않음
+        console.log('shouldComponentUpdate');
+        if (nextState.number % 5 === 0) return false;
+        return true;
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        console.log('componentWillUpdate');
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log('componentDidUpdate');
     }
 
     handleIncrease = () => {
-        this.setState(
-            (state) => ({
-                number: state.number + 1
-            })
-        )
+        const { number } = this.state;
+        this.setState({
+            number: number + 1
+        });
     };
 
     handleDecrease = () => {
         this.setState(
-            (state) => ({
-                number: state.number - 1
+            ({ number }) => ({
+                number: number - 1
             })
-        )
+        );
     };
 
     render() {
+        console.log('render');
         return (
             <div>
                 <h1>카운터</h1>
-                <div>값 : {this.state.number}</div>
+                <div>값: {this.state.number}</div>
                 <button onClick={this.handleIncrease}>+</button>
                 <button onClick={this.handleDecrease}>-</button>
             </div>
-        );
+        )
     }
 }
 
