c20c998b317477fc077e2b7bfef7ed8a7f413660
leesoo <isoo7510@gmail.com>
2018-12-19 11:44:26 +0900

    state

--- src/layout.js
+++ src/layout.js
@@ -4,9 +4,14 @@ import Title from './Header/Title';
 import Footer from './footer';
 
 export default class Layout extends React.Component {
+    constructor() {
+        super();
+        this.state = {name: "Sueleesoossoo"};
+    }
     render() {
         return (
             <div>
+                {this.state.name}
                 <Title />
                 <Footer />
             </div>
