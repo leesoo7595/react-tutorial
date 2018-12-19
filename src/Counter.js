273e176b2ade417b0859194845caa619a3e0e30c
leesoo <isoo7510@gmail.com>
2018-12-19 17:43:05 +0900

    number 4 일때 에러 발생시키기

--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
+const Problematic = () => {
+  throw (new Error('버그'));
+  return (
+      <div>
+
+      </div>
+  );
+};
+
 class Counter  extends React.Component {
     state = {
         number: 0
@@ -54,6 +63,7 @@ class Counter  extends React.Component {
             <div>
                 <h1>카운터</h1>
                 <div>값: {this.state.number}</div>
+                { this.state.number === 4 && <Problematic /> }
                 <button onClick={this.handleIncrease}>+</button>
                 <button onClick={this.handleDecrease}>-</button>
             </div>
