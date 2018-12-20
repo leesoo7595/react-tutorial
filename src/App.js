986e22462218e06c04758116958aa4359ee4cfb9
leesoo <isoo7510@gmail.com>
2018-12-20 11:43:26 +0900

    부모 컴포넌트에게 정보 전달하기

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 import PhoneForm from "./PhoneForm";
 
 class App extends React.Component {
+    handleCreate = (data) => {
+        console.log(data);
+    }
+
     render() {
         return (
             <div>
-                <PhoneForm />
+                <PhoneForm
+                    onCreate={this.handleCreate}
+                />
             </div>
         );
     }
--- src/PhoneForm.js
+++ src/PhoneForm.js
@@ -11,11 +11,22 @@ class PhoneForm extends React.Component {
             [e.target.name]: e.target.value
         });
     };
+    handleSubmit = (e) => {
+        // 페이지 리로딩 방지
+        e.preventDefault();
+        // 상태값을 onCreate를 통하여 부모에게 전달
+        this.props.onCreate(this.state);
+        // 상태 초기화
+        this.setState({
+            name: '',
+            phone: ''
+        })
+    };
     render() {
         return (
             // onChange : input의 텍스트값이 바뀔때마다 발생하는 이벤트(handleChange)
             // value : 데이터 등록 후 name값 초기화 -> 현재의 name값을 반영시키도록 value를 설정
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <input
                     placeholder="이름"
                     value={this.state.name}
@@ -29,7 +40,7 @@ class PhoneForm extends React.Component {
                     onChange={this.handleChange}
                     name="phone"
                 />
-                <div>{this.state.name} {this.state.phone}</div>
+                <button type="submit">등록</button>
             </form>
         );
     }
