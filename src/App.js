52e990001f9b8b8f02652be4cb6d0698e9e69963
leesoo <isoo7510@gmail.com>
2018-12-20 12:00:35 +0900

    array data add

--- src/App.js
+++ src/App.js
@@ -2,16 +2,37 @@ import React from 'react';
 import PhoneForm from "./PhoneForm";
 
 class App extends React.Component {
+    id = 2;
+    state = {
+        information: [
+            {
+                id: 0,
+                name: '천이수',
+                phone: '010-2129-7510'
+            },
+            {
+                id: 1,
+                name: '천이현',
+                phone: '010-6690-7510'
+            }
+        ]
+    };
+
     handleCreate = (data) => {
-        console.log(data);
-    }
+        const { information } = this.state;
+        this.setState({
+            information: information.concat({ id: this.id++, ...data})
+        })
+    };
 
     render() {
+        const { information } = this.state;
         return (
             <div>
                 <PhoneForm
                     onCreate={this.handleCreate}
                 />
+                {JSON.stringify(information)}
             </div>
         );
     }
